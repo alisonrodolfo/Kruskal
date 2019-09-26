ff279554dbaf3105abd34526294294736dbe0015
alisonrodolfo <alison.rmb@gmail.com>
2019-09-26 01:36:39 -0300

    heapsort

---
+++ src/main/java/Model/Nodo.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Alison Rodolfo
+ */
+public class Nodo {
+    private int id;
+    private int x;
+    private int y;
+
+    public Nodo(int id, int x, int y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    
+    
+}
--- src/main/java/com/alisonbarreiro/kruskal/FXMLController.java
+++ src/main/java/com/alisonbarreiro/kruskal/FXMLController.java
@@ -1,6 +1,11 @@
 package com.alisonbarreiro.kruskal;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,17 +14,282 @@ import javafx.scene.control.Label;
 
 public class FXMLController implements Initializable {
     
+    private static ArrayList<Nodo> nodos = new ArrayList<Nodo>();
+    
+    private static final String NAME = "NAME:";
+    private static final String NODES = "DIMENSION:";
+    
+    private String nome;
+    private int NUM_NODES;
+
     @FXML
     private Label label;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+        addInstance("src/main/resources/instances-Kruskal/n1500A.txt");
+        GrafoKruskal g = new GrafoKruskal();
+        g.main();
+        
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    class GrafoKruskal {
+        // A class to represent a graph edge 
+
+        class Edge implements Comparable<Edge> {
+
+            int src, dest, weight;
+
+            // Comparator function used for sorting edges  
+            // based on their weight 
+            public int compareTo(Edge compareEdge) {
+                return this.weight - compareEdge.weight;
+            }
+        };
+
+        // A class to represent a subset for union-find 
+        class subset {
+
+            int parent, rank;
+        };
+
+        int V, E;    // V-> no. of vertices & E->no.of edges 
+        Edge edge[]; // collection of all edges 
+
+        // Creates a graph with V vertices and E edges 
+        GrafoKruskal(int v, int e) {
+            V = v;
+            E = e;
+            edge = new Edge[E];
+            for (int i = 0; i < e; ++i) {
+                edge[i] = new Edge();
+            }
+        }
+        GrafoKruskal() {
+
+        }
+
+        // A utility function to find set of an element i 
+        // (uses path compression technique) 
+        int find(subset subsets[], int i) {
+            // find root and make root as parent of i (path compression) 
+            if (subsets[i].parent != i) {
+                subsets[i].parent = find(subsets, subsets[i].parent);
+            }
+
+            return subsets[i].parent;
+        }
+
+        // A function that does union of two sets of x and y 
+        // (uses union by rank) 
+        void Union(subset subsets[], int x, int y) {
+            int xroot = find(subsets, x);
+            int yroot = find(subsets, y);
+
+            // Attach smaller rank tree under root of high rank tree 
+            // (Union by Rank) 
+            if (subsets[xroot].rank < subsets[yroot].rank) {
+                subsets[xroot].parent = yroot;
+            } else if (subsets[xroot].rank > subsets[yroot].rank) {
+                subsets[yroot].parent = xroot;
+            } // If ranks are same, then make one as root and increment 
+            // its rank by one 
+            else {
+                subsets[yroot].parent = xroot;
+                subsets[xroot].rank++;
+            }
+        }
+
+        // The main function to construct MST using Kruskal's algorithm 
+        void KruskalMST() {
+            Edge result[] = new Edge[V];  // Tnis will store the resultant MST 
+            int e = 0;  // An index variable, used for result[] 
+            int i = 0;  // An index variable, used for sorted edges 
+            for (i = 0; i < V; ++i) {
+                result[i] = new Edge();
+            }
+
+            // Step 1:  Sort all the edges in non-decreasing order of their 
+            // weight.  If we are not allowed to change the given graph, we 
+            // can create a copy of array of edges 
+            Arrays.sort(edge);
+
+            // Allocate memory for creating V ssubsets 
+            subset subsets[] = new subset[V];
+            for (i = 0; i < V; ++i) {
+                subsets[i] = new subset();
+            }
+
+            // Create V subsets with single elements 
+            for (int v = 0; v < V; ++v) {
+                subsets[v].parent = v;
+                subsets[v].rank = 0;
+            }
+
+            i = 0;  // Index used to pick next edge 
+
+            // Number of edges to be taken is equal to V-1 
+            while (e < V - 1) {
+                // Step 2: Pick the smallest edge. And increment  
+                // the index for next iteration 
+                Edge next_edge = new Edge();
+                next_edge = edge[i++];
+
+                int x = find(subsets, next_edge.src);
+                int y = find(subsets, next_edge.dest);
+
+                // If including this edge does't cause cycle, 
+                // include it in result and increment the index  
+                // of result for next edge 
+                if (x != y) {
+                    result[e++] = next_edge;
+                    Union(subsets, x, y);
+                }
+                // Else discard the next_edge 
+            }
+
+            // print the contents of result[] to display 
+            // the built MST 
+            System.out.println("Following are the edges in "
+                    + "the constructed MST");
+            for (i = 0; i < e; ++i) {
+                System.out.println(result[i].src + " -- "
+                        + result[i].dest + " == " + result[i].weight);
+            }
+        }
+
+        // Driver Program 
+        public void main() {
+
+            /* Let us create following weighted graph 
+                 10 
+            0--------1 
+            |  \     | 
+           6|   5\   |15 
+            |      \ | 
+            2--------3 
+                4       */
+            int V = 4;  // Number of vertices in graph 
+            int E = 5;  // Number of edges in graph 
+            GrafoKruskal graph = new GrafoKruskal(V, E);
+
+            // add edge 0-1 
+            graph.edge[0].src = 0;
+            graph.edge[0].dest = 1;
+            graph.edge[0].weight = 10;
+
+            // add edge 0-2 
+            graph.edge[1].src = 0;
+            graph.edge[1].dest = 2;
+            graph.edge[1].weight = 6;
+
+            // add edge 0-3 
+            graph.edge[2].src = 0;
+            graph.edge[2].dest = 3;
+            graph.edge[2].weight = 5;
+
+            // add edge 1-3 
+            graph.edge[3].src = 1;
+            graph.edge[3].dest = 3;
+            graph.edge[3].weight = 15;
+
+            // add edge 2-3 
+            graph.edge[4].src = 2;
+            graph.edge[4].dest = 3;
+            graph.edge[4].weight = 4;
+
+            graph.KruskalMST();
+        }
+    }
+    
+    
+    public void addInstance(String file) {
+
+        System.out.println("LENDO ARQUIVO");
+        try {
+            FileReader arq = new FileReader(file);
+            BufferedReader lerArq = new BufferedReader(arq);
+            String linha = lerArq.readLine();
+            String dados;
+            while (linha != null) {
+                dados = linha.trim().replaceAll("\t", "").replaceAll("\\s+", " ");
+
+                if (dados.startsWith(NAME)) {
+                    //System.out.println("NOME: " + dados.substring(NAME.length()).replaceAll("\\s+", ""));
+                    //textname.setText(dados.substring(NAME.length()).replaceAll("\\s+", ""));
+                    //heuristica.setInstancia(dados.substring(NAME.length()).replaceAll("\\s+", ""));
+                } else if (dados.startsWith(NODES)) {
+                   // System.out.println("DIMENSION: " + dados.substring(DIMENSION.length()).replaceAll("\\s+", ""));
+                    NUM_NODES = Integer.parseInt(dados.substring(NODES.length()).replaceAll("\\s+", ""));
+                    // textdimension.setText(dados.substring(DIMENSION.length()).replaceAll("\\s+", ""));
+                   //textcapacity.setText(dados.substring(CAPACITY.length()).replaceAll("\\s+", ""));
+                } else if (dados.startsWith("DISPLAY_DATA_SECTION")) {
+                    linha = lerArq.readLine();
+                    break;
+                }
+                linha = lerArq.readLine(); // lê da segunda até a última linha
+            }
+
+            for (int i = 0; i < NUM_NODES; i++) {
+
+                dados = linha.trim().replaceAll("\t", "").replaceAll("\\s+", " ");
+                String tokens[] = dados.split(" ");
+                
+                vertices.add(new Vertice(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), false));
+
+                linha = lerArq.readLine();
+            }
+
+            linha = lerArq.readLine();
+            dados = linha.trim().replaceAll("\t", "").replaceAll("\\s+", " ");
+            if (dados.startsWith("NODE_COORD_SECTION:")) {
+                linha = lerArq.readLine();
+
+            }
+
+            for (int i = 0; i < NUM_VERTICE; i++) {
+
+                dados = linha.trim().replaceAll("\t", "").replaceAll("\\s+", " ");
+                String tokens[] = dados.split(" ");
+
+            
+
+                if (tokens[0].equalsIgnoreCase("" + i)) {
+                    vertices.get(i).setX(Integer.parseInt(tokens[1]));
+                    vertices.get(i).setY(Integer.parseInt(tokens[2]));
+                }
+                linha = lerArq.readLine();
+            }
+
+            for (int i = 0; i < NUM_VERTICE; i++) {
+
+                ArrayList<Aresta> arestasFor = new ArrayList<Aresta>();
+
+                for (int j = 0; j < NUM_VERTICE; j++) {
+
+                    arestasFor.add(new Aresta((int) Math.sqrt(Math.pow(vertices.get(j).getX() - vertices.get(i).getX(), 2) + Math.pow(vertices.get(j).getY() - vertices.get(i).getY(), 2)), j));
+
+                }
+
+                arestas.add(arestasFor);
+                linha = lerArq.readLine();
+
+            }
+
+            arq.close();
+        } catch (IOException e) {
+            System.err.printf("Erro na abertura do arquivo: %s.\n",
+                    e.getMessage());
+        } finally {
+
+        }
+    }
+//This code is contributed by Aakash Hasija 
 }
